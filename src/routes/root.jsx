1a987545ed21bba05244564338a0476478884f19
Reisel Valle <reiselvalle@gmail.com>
2023-10-19 11:19:20 -0500

    creating and exporting a load function

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,10 @@
 import { Outlet, Link } from 'react-router-dom'
+import { getContacts } from "../contacts";
+
+export async function loader() {
+  const contacts = await getContacts();
+  return { contacts };
+}
 
 export function Root() {
     return (
