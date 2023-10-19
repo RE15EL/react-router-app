e144c9bef3d3f6594ce3c43e7b58dbe4e1232516
Reisel Valle <reiselvalle@gmail.com>
2023-10-19 11:21:01 -0500

    path connected charging function

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider} from 'react-router-dom'
-import { Root } from './routes/root';
+import { Root, loader as rootLoader } from './routes/root';
 import { ErrorPage } from './routes/error-page';
 import Contact from './routes/contact';
 
@@ -11,6 +11,7 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
     children: [
       {
         path: "/contacts/:contactId",
